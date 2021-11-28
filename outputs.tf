5b9110083c4d663351d251ddb0d02a16663e5f96
Theo Chatzimichos <tampakrap@gmail.com>
2021-11-28 14:36:34 +0100

    Import playlist "Hip Hop tracks with Greek samples"

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "playlist_url_hip_hop_tracks_with_greek_samples" {
+  value       = "https://open.spotify.com/playlist/${spotify_playlist.hip_hop_tracks_with_greek_samples.id}"
+  description = "The URL of the Spotify playlist \"Hip Hop tracks with Greek samples\""
+}
---
+++ playlist_hip_hop_tracks_with_greek_samples.tf
@@ -0,0 +1,321 @@
+data "spotify_search_track" "the_anonymous-manifest_destiny" {
+  artist = "The Anonymous"
+  name   = "Manifest Destiny"
+  album  = "Green and Gold"
+}
+
+data "spotify_search_track" "canibus-spartibus" {
+  artist = "Canibus"
+  name   = "Spartibus"
+  #album  = "Rip The Jacker"
+}
+
+data "spotify_search_track" "canibus-ripperland" {
+  artist = "Canibus"
+  name   = "Ripperland"
+  album  = "Melatonin Magik"
+}
+
+data "spotify_search_track" "dilated_peoples-reach_us" {
+  artist = "Dilated Peoples"
+  name   = "Reach Us"
+  album  = "Neighborhood Watch"
+}
+
+data "spotify_search_track" "dogma_crew-un_detalle_por_mi_parte" {
+  artist = "Dogma Crew"
+  name   = "Un Detalle Por Mi Parte"
+  album  = "Block Massacre"
+}
+
+data "spotify_search_track" "fel_sweetenberg-the_end_of_the_book" {
+  artist = "DJ Brans" # Fel Sweetenberg & DJ Brans
+  name   = "The End Of The Book"
+  album  = "The Invisible Garden"
+}
+
+data "spotify_search_track" "foreign_beggars-slo-speed" {
+  artist = "Foreign Beggars"
+  name   = "Slo-Speed"
+  album  = "Stray Point Agenda"
+}
+
+data "spotify_search_track" "heavy_metal_kings-impaled_nazarene" {
+  artist = "ILL Bill" # Heavy Metal Kings (Ill Bill & Vinnie Paz)
+  name   = "Impaled Nazarene"
+  album  = "Heavy Metal Kings"
+}
+
+data "spotify_search_track" "hieroglyphics-what_the_funk" {
+  artist = "Hieroglyphics"
+  name   = "What The Funk"
+  album  = "LIVE Full Circle Tour"
+}
+
+data "spotify_track" "j-zone-county_check_pimpin" {
+  url = "https://open.spotify.com/track/2zbjAVqR3uSfFabqA2Gbzx"
+}
+
+#data "spotify_search_track" "j-zone-county_check_pimpin" {
+#  artist = "J-Zone"
+#  name   = "County Check Pimpin'"
+#  album  = "Pimps Don't Pay Taxes"
+#}
+
+data "spotify_track" "j-zone-county_check_pimpin_remix" {
+  url = "https://open.spotify.com/track/5986IkoP5J0u6GAl5Qpmjf"
+}
+
+#data "spotify_search_track" "j-zone-county_check_pimpin_remix" {
+#  artist = "J-Zone"
+#  name   = "County Check Pimpin' (Remix)"
+#  album  = "Old Maid Looses EP (2001-2008)"
+#}
+
+data "spotify_search_track" "jedi_mind_tricks-blood_in_blood_out" {
+  artist = "Jedi Mind Tricks"
+  name   = "Blood In Blood Out"
+  album  = "Visions Of Gandhi"
+}
+
+data "spotify_search_track" "jedi_mind_tricks-monolith" {
+  artist = "Jedi Mind Tricks"
+  name   = "Monolith"
+  album  = "A History Of Violence"
+}
+
+data "spotify_search_track" "jedi_mind_tricks-no_jesus_no_beast" {
+  artist = "Jedi Mind Tricks"
+  name   = "No Jesus, No Beast"
+  album  = "The Thief And The Fallen"
+}
+
+data "spotify_search_track" "living_legends-moving_at_the_speed_of_life" {
+  artist = "Living Legends"
+  name   = "Moving At The Speed Of Life"
+  album  = "Legendary Music, Vol.1"
+}
+
+data "spotify_search_track" "the_lost_children_of_babylon-skull_and_bones" {
+  artist = "The Lost Children Of Babylon"
+  name   = "Skull & Bones"
+  album  = "Zeitgeist: The Spirit Of The Age"
+}
+
+data "spotify_search_track" "the_loyalists-dollar_child" {
+  artist = "The Loyalists"
+  name   = "Dollar Child"
+  album  = "Moodswing"
+}
+
+data "spotify_search_track" "main_flow-hip_hop_worth_dying_for" {
+  artist = "Main Flow"
+  name   = "Hip Hop Worth Dying For"
+  album  = "Hip Hopulation"
+}
+
+data "spotify_search_track" "masta_ace-somethings_wrong" {
+  artist = "Masta Ace"
+  name   = "Something's Wrong"
+  album  = "Disposable Arts"
+}
+
+data "spotify_search_track" "mathematics-rush" {
+  artist = "Mathematics"
+  name   = "Rush"
+  album  = "The Problem"
+}
+
+data "spotify_search_track" "mr_lif-head_high" {
+  artist = "Mr. Lif"
+  name   = "Head High"
+  album  = "I Heard It Today"
+}
+
+data "spotify_search_track" "nature-we_aint_friends" {
+  artist = "Nature"
+  name   = "We Ain't Friends"
+  album  = "For All Seasons"
+}
+
+data "spotify_search_track" "necro-horny_honeys" {
+  artist = "Necro"
+  name   = "Horny Honeys"
+  album  = "The Sexorcist"
+}
+
+data "spotify_search_track" "pavel_baar-vasen" {
+  artist = "Pavel Baar"
+  name   = "Vášeň"
+  album  = "Pavel Baar - Pár Let 2006"
+}
+
+data "spotify_search_track" "pavel_baar-byt_sam" {
+  artist = "Pavel Baar"
+  name   = "Být Sám"
+  album  = "Pavel Baar - Pár Let 2006"
+}
+
+data "spotify_search_track" "pavel_baar-estat_de_locativo_matador" {
+  artist = "Pavel Baar"
+  name   = "Estat De Locativo Matador"
+  album  = "Pavel Baar - Pár Let 2006"
+}
+
+data "spotify_search_track" "pavel_baar-rozbourane_cesty" {
+  artist = "Pavel Baar"
+  name   = "Rozbourané Cesty"
+  album  = "Pavel Baar - Pár Let 2006"
+}
+
+data "spotify_search_track" "porta-entre_avenidas_y_aceras" {
+  artist = "Porta"
+  name   = "Entre Avenidas Y Aceras"
+  album  = "No Es Cuestión De Edades"
+}
+
+data "spotify_search_track" "promoe-heartburns" {
+  artist = "Promoe"
+  name   = "Heartburns"
+  album  = "Standard Bearer"
+}
+
+data "spotify_search_track" "qb_finest-kids_in_da_pjs" {
+  artist = "Nas" # QB Finest
+  name   = "Kids In Da PJ's"
+  album  = "Queensbridge The Album"
+}
+
+data "spotify_search_track" "raekwon-jury" {
+  artist = "Raekwon"
+  name   = "Jury"
+  album  = "Immobilarity"
+}
+
+data "spotify_search_track" "raekwon-live_from_new_york" {
+  artist = "Raekwon"
+  name   = "Live From New York"
+  album  = "Immobilarity"
+}
+
+data "spotify_search_track" "shoestring-so_high" {
+  artist = "Shoestring"
+  name   = "So High"
+  album  = "The Right To Remain Silent EP"
+}
+
+data "spotify_search_track" "sick_jacken-metal_rain" {
+  artist = "Sick Jacken"
+  name   = "Metal Rain"
+  album  = "Stray Bullets"
+}
+
+data "spotify_search_track" "snowgoons-black_woods" {
+  artist = "Snowgoons"
+  name   = "Black Woods"
+  album  = "German Lugers"
+}
+
+data "spotify_search_track" "snowgoons-the_ill_bunch" {
+  artist = "Snowgoons"
+  name   = "The Ill Bunch"
+  album  = "The Trojan Horse"
+}
+
+data "spotify_search_track" "snowgoons-knockatomi_plaza" {
+  artist = "Snowgoons"
+  name   = "Knockatomi Plaza"
+  album  = "Black Snow"
+}
+
+data "spotify_search_track" "sole-selling_live_water" {
+  artist = "Sole"
+  name   = "Selling Live Water"
+  album  = "Selling Live Water"
+}
+
+data "spotify_search_track" "street_platoon-drug_lab" {
+  artist = "Street Platoon"
+  name   = "Drug Lab"
+  album  = "The Steel Storm"
+}
+
+data "spotify_search_track" "task_force-so_loco" {
+  artist = "Task Force"
+  name   = "So Loco"
+  album  = "Music From The Corner: Volume 2"
+}
+
+data "spotify_search_track" "vinnie_paz-paul_and_paz" {
+  artist = "Vinnie Paz"
+  name   = "Paul And Paz"
+  album  = "Season Of The Assassin"
+}
+
+data "spotify_search_track" "yellowjackets-roll_with_killer_bees" {
+  artist = "Yellowjackets" # Wu-Tang Killa Bees
+  name   = "Roll WIth Killer Bees"
+  album  = "Pollen: The Swarm, Pt.3"
+}
+
+data "spotify_search_track" "yukmouth-be_eazy" {
+  artist = "Yukmouth"
+  name   = "Be Eazy"
+  album  = "Godzilla"
+}
+
+resource "spotify_playlist" "hip_hop_tracks_with_greek_samples" {
+  name        = "Hip Hop tracks with Greek samples"
+  public      = true
+
+  tracks = [
+    data.spotify_search_track.the_anonymous-manifest_destiny.tracks[0].id,
+    data.spotify_search_track.canibus-spartibus.tracks[0].id,
+    data.spotify_search_track.canibus-spartibus.tracks[2].id,
+    data.spotify_search_track.canibus-spartibus.tracks[1].id,
+    data.spotify_search_track.canibus-spartibus.tracks[4].id,
+    data.spotify_search_track.canibus-ripperland.tracks[0].id,
+    data.spotify_search_track.dilated_peoples-reach_us.tracks[0].id,
+    data.spotify_search_track.dogma_crew-un_detalle_por_mi_parte.tracks[0].id,
+    data.spotify_search_track.fel_sweetenberg-the_end_of_the_book.tracks[0].id,
+    data.spotify_search_track.foreign_beggars-slo-speed.tracks[0].id,
+    data.spotify_search_track.heavy_metal_kings-impaled_nazarene.tracks[0].id,
+    data.spotify_search_track.hieroglyphics-what_the_funk.tracks[0].id,
+    data.spotify_track.j-zone-county_check_pimpin.id,
+    #data.spotify_search_track.j-zone-county_check_pimpin.tracks[0].id,
+    data.spotify_track.j-zone-county_check_pimpin_remix.id,
+    #data.spotify_search_track.j-zone-county_check_pimpin_remix.tracks[0].id,
+    data.spotify_search_track.jedi_mind_tricks-blood_in_blood_out.tracks[0].id,
+    data.spotify_search_track.jedi_mind_tricks-monolith.tracks[0].id,
+    data.spotify_search_track.jedi_mind_tricks-no_jesus_no_beast.tracks[0].id,
+    data.spotify_search_track.living_legends-moving_at_the_speed_of_life.tracks[0].id,
+    data.spotify_search_track.the_lost_children_of_babylon-skull_and_bones.tracks[0].id,
+    data.spotify_search_track.the_loyalists-dollar_child.tracks[0].id,
+    data.spotify_search_track.main_flow-hip_hop_worth_dying_for.tracks[0].id,
+    data.spotify_search_track.masta_ace-somethings_wrong.tracks[0].id,
+    data.spotify_search_track.mathematics-rush.tracks[0].id,
+    data.spotify_search_track.mr_lif-head_high.tracks[0].id,
+    data.spotify_search_track.nature-we_aint_friends.tracks[0].id,
+    data.spotify_search_track.necro-horny_honeys.tracks[0].id,
+    data.spotify_search_track.pavel_baar-vasen.tracks[0].id,
+    data.spotify_search_track.pavel_baar-byt_sam.tracks[0].id,
+    data.spotify_search_track.pavel_baar-estat_de_locativo_matador.tracks[0].id,
+    data.spotify_search_track.pavel_baar-rozbourane_cesty.tracks[0].id,
+    data.spotify_search_track.porta-entre_avenidas_y_aceras.tracks[0].id,
+    data.spotify_search_track.promoe-heartburns.tracks[0].id,
+    data.spotify_search_track.qb_finest-kids_in_da_pjs.tracks[0].id,
+    data.spotify_search_track.raekwon-jury.tracks[0].id,
+    data.spotify_search_track.raekwon-live_from_new_york.tracks[0].id,
+    data.spotify_search_track.shoestring-so_high.tracks[0].id,
+    data.spotify_search_track.sick_jacken-metal_rain.tracks[0].id,
+    data.spotify_search_track.snowgoons-black_woods.tracks[0].id,
+    data.spotify_search_track.snowgoons-the_ill_bunch.tracks[0].id,
+    data.spotify_search_track.snowgoons-knockatomi_plaza.tracks[0].id,
+    data.spotify_search_track.sole-selling_live_water.tracks[0].id,
+    data.spotify_search_track.street_platoon-drug_lab.tracks[0].id,
+    data.spotify_search_track.task_force-so_loco.tracks[0].id,
+    data.spotify_search_track.vinnie_paz-paul_and_paz.tracks[0].id,
+    data.spotify_search_track.yellowjackets-roll_with_killer_bees.tracks[0].id,
+    data.spotify_search_track.yukmouth-be_eazy.tracks[0].id,
+  ]
+}
